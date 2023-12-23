baaa8ce534246ada6ad85138fa186a707483aca3
Fabio <fabio.garcia@ynov.com>
2023-12-23 01:53:20 +0100

    finished roadbookcard

--- liberty-rider-roadbooks-clone-JS/src/components/roadbook/roadbookCard.jsx
+++ liberty-rider-roadbooks-clone-JS/src/components/roadbook/roadbookCard.jsx
@@ -1,7 +1,7 @@
 export const RoadbookCard = ({ roadbook, tmp_url }) => {
     console.log(roadbook)
     return (
-        <div className="card w-[294px] bg-base-100 shadow-xl">
+        <div className="card w-[294px] bg-base-100 shadow-md">
             <div className="card-body m-3 p-2">
                 <div className="flex items-center">
                     <img className="w-6 rounded-full" src={roadbook.owner_photo_url} />
@@ -15,20 +15,20 @@ export const RoadbookCard = ({ roadbook, tmp_url }) => {
                     <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
                     <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
                 </div>
-                <div className="card bg-base-100 shadow-xl">
+                <div className="card bg-base-100 border border-gray-300 rounded-lg">
                     <figure><img src={tmp_url} alt="map thumbmail" /></figure>
-                    <div className="flex place-content-around">
+                    <div className="flex place-content-around my-2">
                         <div>
                             <div className="text-gray-400 text-xs">Distance</div>
-                            <div className="text-sm"> {Math.round((roadbook.distance / 1000))} Km</div>
+                            <div className="text-sm font-semibold"> {Math.round((roadbook.distance / 1000))} Km</div>
                         </div>
                         <div>
                             <div className="text-gray-400 text-xs">Durée</div>
-                            <div className="text-sm"> {Math.floor(roadbook.duration / 3600)}h{Math.round(roadbook.duration % 60)}</div>
+                            <div className="text-sm font-semibold"> {Math.floor(roadbook.duration / 3600)}h{Math.round(roadbook.duration % 60)}</div>
                         </div>
                         <div>
                             <div className="text-gray-400 text-xs">Niveau</div>
-                            <div className="text-sm"> {roadbook.suggested_level}</div>
+                            <div className="text-sm font-semibold"> {roadbook.suggested_level}</div>
                         </div>
                     </div>
                 </div>
